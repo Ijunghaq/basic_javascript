74650979252bc6094152839a248693d21def6d64
Ijunghaq <134781958+Ijunghaq@users.noreply.github.com>
2024-01-19 21:49:24 +0700

    Add files via upload

---
+++ Angkot .js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<body>
+
+		<h1>JavaScript CompilerResponse</h1>
+
+		<script> 
+			document.write("<p>This is a paragraph</p>");
+		document.write("<h2>ya Alloh semoga bermanfaat, aamiin 😆😄</h2>");
+			var jmlAngkot =10;
+			var angkotBeroprasi = 6;
+			var noAngkot;
+			
+			 
+			 	for(  noAngkot = 1; noAngkot <= jmlAngkot;  noAngkot++){  
+			 		if (noAngkot <= angkotBeroprasi)
+			 	        document.write ('Angkot No. '+noAngkot+' Beroperasi Dengan Baik. <br>');
+			 		}
+		</script>
+	</body>
+</html>
---
+++ Angkot.js
@@ -0,0 +1,15 @@
+	<script> 
+			document.write("<p>This is a paragraph</p>");
+		document.write("<h2>ya Alloh semoga bermanfaat, aamiin 😆😄</h2>");
+			var jmlAngkot =10;
+			var angkotBeroprasi = 6;
+			var noAngkot = 1;
+			
+			 while( noAngkot <= angkotBeroprasi )  {
+			 	document.write ('Angkot No. '+noAngkot+' Beroperasi Dengan Baik. <br>');
+			 	noAngkot++;
+			 	}
+			 	for(  noAngkot = angkotBeroprasi+1; noAngkot <= jmlAngkot;  noAngkot++){  
+			 		document.write ('Angkot No. '+noAngkot+' Tidak Beroperasi Dengan Baik. <br>');
+			 		}
+		</script>
